ef63633d1fa87c20848ca9d67bd0b85be947cbe4
20-marlie-01 <lievanomarcos9@gmail.com>
2025-06-12 13:29:19 -0600

    Arrow finished

--- draw_shapes.c
+++ draw_shapes.c
@@ -26,3 +26,25 @@ void print_triangle(int leftCol, int size)
   }
 }
 
+//Prints an arrow of the specified height.
+void print_arrow(int leftCol, int size)
+{
+  for (int row = 0; row <= size; row++) {
+    int minCol = leftCol + size - row, maxCol = leftCol + size + row;
+    int col;
+    for (col = 4; col < minCol; col++) putchar(' ');
+    for ( ; col <= maxCol; col++) putchar('*');
+    putchar('\n');
+  }
+  
+  int i, j;
+  int endCol = leftCol + size;
+  for (int row = 0; row < size; row++){
+    int col;
+    for (col = 0; col < leftCol; col++) putchar(' ');
+    for ( ; col < endCol; col++) putchar('*');
+    putchar('\n');
+  }
+}
+
+
--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrow = a, Chars = c) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -23,6 +23,10 @@ int main()
     case 's':
       puts("You selected square:");
       print_square(5, 5);
+      break;
+    case 'a':
+      puts("You selected arrow:");
+      print_arrow(5, 7);
       break;
     case 'c':
       puts("You selected chars:");
